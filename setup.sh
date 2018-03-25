ae5fb34ab4b940a2078d18f9bfb11ed9351725c2
LYSYyy <33262838+LYSYyy@users.noreply.github.com>
2018-03-25 14:08:25 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# SETUP SCRIPT by LYSYyy v1.0
+# SETUP SCRIPT by LYSYyy v1.1
 
 	echo
 	echo -e " \e[1;32m[+]\e[1;39m FIRST CONFIGURE...\e[0m"
@@ -9,10 +9,12 @@
 	read -e lang
 	echo
 	if [ $lang = PL ]; then
+		chmod +x ./mfc/PL/*
 		cp ./mfc/PL/* /bin
 		source mfc
 		
 	elif [ $lang = EN ]; then
+		chmod +x ./mfc/PL/*
 		cp ./mfc/EN/* /bin
 		source mfc
 		
