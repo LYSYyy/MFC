60bb114afbbfe66212c3e1f41c540c3c90d2ad36
LYSYyy <33262838+LYSYyy@users.noreply.github.com>
2018-04-15 05:26:35 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,24 +2,37 @@
 
 # SETUP SCRIPT by LYSYyy v1.1
 
+function main
+{
 	echo
 	echo -e " \e[1;32m[+]\e[1;39m FIRST CONFIGURE...\e[0m"
 	echo
 	echo -ne "Choose language (PL / EN): "
 	read -e lang
 	echo
-	if [ $lang = PL ]; then
+	if [ $lang = "PL" ]; then
 		chmod +x ./mfc/PL/*
 		cp ./mfc/PL/* /bin
 		source mfc
 		
-	elif [ $lang = EN ]; then
+	if [ $lang = "pl" ]; then
+		chmod +x ./mfc/PL/*
+		cp ./mfc/PL/* /bin
+		source mfc
+		
+	elif [ $lang = "EN" ]; then
+		chmod +x ./mfc/PL/*
+		cp ./mfc/EN/* /bin
+		source mfc
+		
+	elif [ $lang = "en" ]; then
 		chmod +x ./mfc/PL/*
 		cp ./mfc/EN/* /bin
 		source mfc
 		
 	else
-		echo -e "\e[1;31mError\e[0m"
-		exit
+		main
 	fi
-	exit
+}
+
+main
